0129b608686a98d242738b67afe9504446dfcfd9
pysouth dj <mrngondji@gmail.com>
2021-12-01 19:33:37 -0500

    add the function to pull prices

--- project_functions.py
+++ project_functions.py
@@ -5,10 +5,12 @@ def get_price(asset,from_date_var,to_date_var,interval_var):
     """
     import san 
     
+
     df = san.get(
         "prices/"+asset,
         from_date=from_date_var,
         to_date=to_date_var,
         interval= interval_var    )
+
 
     return df
